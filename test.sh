3ed7b1be338504d4c61451e0f56a014c32d7f1d1
dseum22 <dseum22@gmail.com>
2023-02-22 21:32:46 -0500

    Updated test

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+declare -a dim=("0" "2" "3" "4")
 declare -a arr=("28" "256" "512" "1024" "2048" "4096" "8192" "16384" "32768" "65536" "131072" "262144")
 
 make
 true > tests.log
 
-for i in "${arr[@]}"
+for i in "${dim[@]}"
 do
-    ./randmst 1 ${i} 5 0 >> tests.log
-    echo $'\n' >> tests.log
+    for j in "${arr[@]}"
+    do
+        ./randmst 1 ${j} 5 ${i} >> tests.log
+        echo $'\n' >> tests.log
+    done
+    echo $'\n'$'\n'
 done
